75ea0ad4cefb61fa95e0a5b17eb7c712cc62710f
rlaekwjd6412 <37580495+rlaekwjd6412@users.noreply.github.com>
2022-06-18 00:48:12 +0900

    PlaceVO 작성
    PlaceVO 작성

---
+++ ddobaki/src/main/java/com/example/demo/vo/PlaceVO.java
@@ -0,0 +1,27 @@
+package com.example.demo.vo;
+
+import lombok.Data;
+
+@Data
+public class PlaceVO {
+	private int place_num;
+	private int place_region_num;
+	private int place_type_num;
+	private String place_name;
+	private String place_explain;
+	private String place_addr;
+	private String place_site;
+	private int place_weight;
+	private String place_tel;
+	private int place_park;
+	private int place_spa;
+	private int place_meal;
+	private int place_parking_price;
+	private int place_spa_price;
+	private int place_meal_price;
+	private String place_thumbnail;
+	private String place_detail;
+	private int place_hit;
+	private String place_lat;
+	private String place_lng;
+}
