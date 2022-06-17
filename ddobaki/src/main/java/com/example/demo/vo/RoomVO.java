460dd4634b6fab01d74d7500b37650aa2b492d92
rlaekwjd6412 <37580495+rlaekwjd6412@users.noreply.github.com>
2022-06-18 00:46:20 +0900

    변수 이름 소문자로 변경
    변수 이름 소문자로 변경

--- ddobaki/src/main/java/com/example/demo/vo/RoomVO.java
+++ ddobaki/src/main/java/com/example/demo/vo/RoomVO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class RoomVO {
-	 private int ROOM_NUM;
-	 private String ROOM_NAME;
-	 private int ROOM_PRICE;
-	 private int PLACE_NUM;
+	 private int room_num;
+	 private String rooom_name;
+	 private int room_price;
+	 private int place_num;
 }
